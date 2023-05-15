d9ee2519b28d6337b3ae10a8612eba0e446cc0f2
Медянцев Кирилл Михайлович <kirill.medyantsev@domrf.ru>
2023-05-15 20:01:25 +0300

    add webpack, eslint & typescript

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React, { StrictMode } from 'react';
+
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement);
+
+root.render(
+  <StrictMode>
+    Hello world!
+  </StrictMode>,
+);
