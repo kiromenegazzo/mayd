5a453bf89689edab291c9d4a6f65e7598298cfd8
Медянцев Кирилл Михайлович <kirill.medyantsev@domrf.ru>
2023-05-16 13:22:50 +0300

    fix ThemeToggler

--- src/features/ui/components/ThemeToggler/index.tsx
+++ src/features/ui/components/ThemeToggler/index.tsx
@@ -1,13 +1,16 @@
-import { MoonOutlineIcon } from 'features/ui/components/Icon';
 import { useTheme } from 'features/ui/hooks/useTheme';
+
+import * as UI from './styles';
 
 export const ThemeToggler = () => {
   const [theme, setTheme] = useTheme();
 
+  const handleClick = () => setTheme(prev => prev === 'light' ? 'dark' : 'light');
+
   return (
-    <>
+    <UI.Toggler onClick={handleClick}>
+      <UI.MoonIcon fill={theme === 'dark' ? 'currentColor' : 'none'}/>
       {`${theme === 'light' ? 'light' : 'dark'} mode`}
-      <MoonOutlineIcon color={theme === 'light' ? 'red' : 'blue'} onClick={() => setTheme(prev => prev === 'light' ? 'dark' : 'light')}/>
-    </>
+    </UI.Toggler>
   );
 };
---
+++ src/features/ui/components/ThemeToggler/styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+import { lightTheme } from 'features/ui/assets/styles';
+import { MoonOutlineIcon } from 'features/ui/components/Icon';
+
+export const Toggler = styled.button
+  .attrs({
+    type: 'button',
+  })`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+  border: none;  
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+export const MoonIcon = styled(MoonOutlineIcon)
+  .withConfig({
+    shouldForwardProp: (prop) => !['theme'].includes(prop),
+  })`
+  width: 20px;
+  height: 20px;
+  background-color: ${({ theme }) => theme.background};
+  color: ${({ theme }) => theme.text};
+`;
+
+MoonIcon.defaultProps = {
+  theme: lightTheme,
+};
