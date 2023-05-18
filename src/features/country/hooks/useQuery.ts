2eb4a7592ca44cb7be26d884cd3bfde271c4ee45
Медянцев Кирилл Михайлович <kirill.medyantsev@domrf.ru>
2023-05-18 15:20:38 +0300

    add useQuery

---
+++ src/features/country/hooks/useQuery.ts
@@ -0,0 +1,40 @@
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+
+import { fetchList, fetchItem, IFetchItemParams, IFetchListParams } from 'features/country/api';
+import { ICountryBase, ICountryDetail } from 'features/country/types/entities';
+
+export const useListQuery = (
+  params?: IFetchListParams,
+  options?: UseQueryOptions<ICountryBase[], AxiosError, ICountryBase[]>,
+) => useQuery({
+  ...options,
+  queryKey: ['countryList', params],
+  queryFn: async () => {
+    try {
+      const response = await fetchList(params);
+
+      return response.data || [];
+    } catch {
+      return [];
+    }
+  },
+});
+
+export const useItemQuery = (
+  params: IFetchItemParams,
+  options?: UseQueryOptions<ICountryDetail[], AxiosError, ICountryDetail[]>,
+) => useQuery({
+  ...options,
+  queryKey: ['countryItem', params.name],
+  queryFn: async () => {
+    try {
+      const response = await fetchItem(params);
+
+      return response.data;
+    } catch {
+      return null;
+    }
+  },
+});
+
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 
 import { RouterProvider } from 'react-router-dom';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createRoot } from 'react-dom/client';
 import { StyleSheetManager } from 'styled-components';
 
@@ -12,13 +14,26 @@ import { Styles, ThemeProvider } from 'features/ui';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement as HTMLDivElement);
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+      refetchOnWindowFocus: false,
+      useErrorBoundary: true,
+    },
+  },
+});
+
 root.render(
   <StrictMode>
     <ErrorBoundary>
       <StyleSheetManager disableVendorPrefixes={process.env.NODE_ENV === 'development'}>
         <ThemeProvider >
           <Styles/>
-          <RouterProvider router={router}/>
+          <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router}/>
+            <ReactQueryDevtools/>
+          </QueryClientProvider>
         </ThemeProvider>
       </StyleSheetManager>
     </ErrorBoundary>
