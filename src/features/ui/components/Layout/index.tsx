4a8466e284cd98fce6e93ff0b26f2f445435029e
Медянцев Кирилл Михайлович <kirill.medyantsev@domrf.ru>
2023-05-18 10:18:33 +0300

    fix Layout

--- src/features/ui/components/Layout/index.tsx
+++ src/features/ui/components/Layout/index.tsx
@@ -2,11 +2,15 @@ import { Outlet } from 'react-router-dom';
 
 import { Header } from 'features/ui';
 
+import * as UI from './styles';
+
 export const Layout = () => {
   return (
     <>
       <Header/>
-      <Outlet />
+      <UI.Container>
+        <Outlet />
+      </UI.Container>
     </>
   );
 };
---
+++ src/features/ui/components/Layout/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+import { EMaxWidth } from 'features/ui/assets/styles';
+
+export const Container = styled.main`
+  max-width: ${EMaxWidth.Desktop}px;
+  margin-right: auto;
+  margin-left: auto;
+  padding: 40px 16px;
+`;
