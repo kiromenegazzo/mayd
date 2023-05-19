8a30c24012a3754a1e698cc69f20426be6f099a8
Медянцев Кирилл Михайлович <kirill.medyantsev@domrf.ru>
2023-05-19 13:31:30 +0300

    fix Layout

---
+++ src/features/ui/components/Layout/__tests__/index.spec.tsx
@@ -0,0 +1,32 @@
+import { render } from 'features/core';
+
+import { Layout } from '../index';
+
+const headerTestId = 'header';
+const outletTestId = 'outlet';
+
+jest.mock('features/ui/components/Header', () => ({
+  Header: () => <div data-testid={headerTestId}/>,
+}));
+
+jest.mock('react-router-dom', () => ({
+  Outlet: () => <div data-testid={outletTestId}/>,
+}));
+
+describe('Component: Layout', () => {
+  it('should render the header', () => {
+    const { getByTestId } = render(<Layout/>);
+
+    const headerElement = getByTestId(headerTestId);
+
+    expect(headerElement).toBeInTheDocument();
+  });
+
+  it('should render the outlet', () => {
+    const { getByTestId } = render(<Layout/>);
+
+    const outletElement = getByTestId(outletTestId);
+
+    expect(outletElement).toBeInTheDocument();
+  });
+});
